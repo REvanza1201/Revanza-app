f4ac8c9223b1502243c5651337f24e8d32a9fe97
REvanza1201 <revanza7c@gmail.com>
2025-04-24 11:01:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("LATIHAN MEMBUAT WEBSITE")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Website punya Revanza Rafi X-B SMAN 20 BANDUNG"
 )
